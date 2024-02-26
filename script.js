44993ec318a8a0bdedb79e81766f565bf15f9812
Ritesh Kishor Chaudhari <100477678+Ritu0097@users.noreply.github.com>
2024-02-26 23:14:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+function findDOMLevel(elementId) {
+    var element = document.getElementById('level');
+    var level = 0;
+    while (element !== null && element.tagName.toLowerCase() !== 'level') {
+        level++; 
+        element = element.parentElement; 
+    }
+    alert("The level of the element is: " + level);
+}
+findDOMLevel('level');
